a9d3517e8f8a1805f6d7e7a07460f15fee8a04a8
carret1268 <carret1268@gmail.com>
2025-07-06 23:03:19 -0400

    Updated CD pipline name. Actually incremented version number in pyproject.toml

--- docs/conf.py
+++ docs/conf.py
@@ -13,7 +13,7 @@ sys.path.insert(0, os.path.abspath('..'))
 project = 'ArrowETC'
 copyright = '2025, E. Tyler Carr'
 author = 'E. Tyler Carr'
-release = '0.0.2.dev1'
+release = '0.1.1'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
